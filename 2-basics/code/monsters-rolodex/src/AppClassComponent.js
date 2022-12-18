6404e157d0a1230629ee2d609a0bd094a671ea8d
Nishant Ingle <nishantingle5@gmail.com>
2022-12-18 22:53:05 +0530

    [3-29] setState

---
+++ 2-basics/code/monsters-rolodex/src/AppClassComponent.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class AppClassComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      'name': 'Nishant',
+    };
+    this.dummy = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          Hi {this.state.name} !
+          <button
+            onClick={() => {
+              // Re-render X
+              // this.state.name = 'New Value';
+
+              // Re-render X
+              // this.state = {
+              //   name: 'Real new value',
+              // };
+
+              // Re-render -> Yes
+              this.setState({ name: 'Final new value !' });
+              console.log(this.state.name);
+            }}
+          >
+            Change name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default AppClassComponent;
