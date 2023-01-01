bef865e5052fb36f2deb32bad4ae2aa2889abc64
Nishant Ingle <nishantingle5@gmail.com>
2023-01-02 00:52:34 +0530

    [3-55] functional components intro

---
+++ 2-basics/code/monsters-rolodex/src/AppFunctionalComponent.js
@@ -0,0 +1,74 @@
+import './App.css';
+import { Component } from 'react';
+import CardList from './components/card-list/card-list.component.jsx';
+import SearchBox from './components/search-box/search-box.component.jsx';
+
+
+const AppFunctionalComponent = () => {
+  return (
+    <div className="App">
+      <h1 className='app-title'>Monsters Rolodex</h1>
+
+      {/* <SearchBox
+        myClassName='monster-search-box'
+        myPlaceholder='search monsters'
+        myOnChangeHandler={ onSearchChange } />
+      <CardList filteredMonsters={filteredMonsters} /> */}
+        
+    </div>
+  );
+}
+
+/*
+class AppClassComponent extends Component {
+
+  constructor() {
+    super();
+    this.state = { monsters: [], searchField: ''};
+  }
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url)
+    .then(resp => {
+      return resp.json();
+    })
+    .then(users => {
+      this.setState(() => {
+        return { monsters: users, searchField: '' };
+      },
+      () => {
+      })
+    });
+  }
+
+  onSearchChange = (event) => {
+    this.setState(() => ({ searchField: event.target.value.toLowerCase() }));
+  };
+
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter(
+      (monster) => monster.name
+                          .toLowerCase()
+                          .includes(searchField));
+
+    return (
+      <div className="App">
+        <h1 className='app-title'>Monsters Rolodex</h1>
+
+        <SearchBox
+          myClassName='monster-search-box'
+          myPlaceholder='search monsters'
+          myOnChangeHandler={ onSearchChange } />
+        <CardList filteredMonsters={filteredMonsters} />
+          
+      </div>
+    );
+  }
+}*/
+
+export default AppFunctionalComponent;
--- 2-basics/code/monsters-rolodex/src/index.js
+++ 2-basics/code/monsters-rolodex/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import AppClassComponent from './AppClassComponent';
+import AppFunctionalComponent from './AppFunctionalComponent';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AppClassComponent />
+    <AppFunctionalComponent />
 );
 
 // If you want to start measuring performance in your app, pass a function
