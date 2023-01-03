2b32e4bd1ab203b7cecc832d0246c9c40f477f6f
Nishant Ingle <nishantingle5@gmail.com>
2023-01-04 00:33:08 +0530

    [3-66] react dom 2

---
+++ 2-basics/66-rdom2/pure-react/app.js
@@ -0,0 +1,30 @@
+const Person = (props) => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {}, props.name),
+    React.createElement('p', {}, props.occupation)
+  ]);
+};
+
+const App = () => {
+  return React.createElement('div', {}, [
+    React.createElement('h1', {className: 'title'}, "React is rendered"),
+    React.createElement(
+      Person,
+      { name: 'abc', occupation: 'first'},
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: 'def', occupation: 'second'},
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: 'ghi', occupation: 'third'},
+      null
+    )
+  ]);
+
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
