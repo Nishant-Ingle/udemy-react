3a222f8db3228f236ad941e291877ffebbc273ee
Nishant Ingle <nishantingle5@gmail.com>
2023-01-03 23:49:54 +0530

    [3-62] react 18 changes

--- 2-basics/code/monsters-rolodex/src/index.js
+++ 2-basics/code/monsters-rolodex/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import AppFunctionalComponent from './AppFunctionalComponent';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
     <AppFunctionalComponent />
 );
