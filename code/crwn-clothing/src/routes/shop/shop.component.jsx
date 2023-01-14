1f1ff5a3c488a79470ab0120c93aa1e724704293
Nishant Ingle <nishantingle5@gmail.com>
2023-01-14 22:16:12 +0530

    [9-112] add shop data

--- code/crwn-clothing/src/routes/shop/shop.component.jsx
+++ code/crwn-clothing/src/routes/shop/shop.component.jsx
@@ -1,11 +1,19 @@
 import { useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
+import SHOP_DATA from "../../data/shop.json";
 
 const Shop = () => {
-  const [setHasShopped] = useContext(UserContext);
-  setHasShopped(true);
-
-  return "I am shop component"
+  return (
+    <div>
+      {
+        SHOP_DATA.map(({ id, name }) => (
+          <div key={ id }>
+            <h1> { name } </h1>
+          </div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default Shop;
